12b25de7fae3511b987c71568b1c4a6c7ec531b8
Prasann Shimpi <prasann@Prasanns-MacBook-Pro.local>
2025-01-19 19:54:53 -0700

    bug-fixes

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-80bf680411bb123a6014.js"
+    "url": "webpack-runtime-b2a2010740de9d3a6a3c.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5297ec37ab0e4c5088e3ab4b876fc781"
+    "revision": "442ad2841c5b7ae16f4f89250208c7e3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
